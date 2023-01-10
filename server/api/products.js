ee4edb7096fe82b15268fcd8e95cb65e13b4867d
Bob <robert.dale.boles@gmail.com>
2023-01-10 10:24:30 -0700

    added some routes

--- server/api/products.js
+++ server/api/products.js
@@ -1,64 +1,52 @@
-const router = require('express').Router()
-const { models: { Product }} = require('../db')
+const router = require("express").Router();
+const {
+    models: { Product },
+} = require("../db");
 
-router.get('/', async (req, res, next) => {
-  try {
-    const products = await Product.findAll({
-      // explicitly select only the id and Productname fields - even though
-      // products' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      attributes: ['id', 'Productname']
-    })
-    res.json(products)
-  } catch (err) {
-    next(err)
-  }
-})
+router.get("/", async (req, res, next) => {
+    try {
+        const products = await Product.findAll({});
+        res.json(products);
+    } catch (err) {
+        next(err);
+    }
+});
 
-router.get('/', async (req, res, next) => {
-    try{
-        const products = await Product.findAll({})
-        res.send(products)
-    } catch (err){
-        next (err)
+router.get("/:id", async (req, res, next) => {
+    try {
+        const products = await Product.findByPk(req.params.id);
+        res.send(products);
+    } catch (err) {
+        next(err);
     }
-})
-  
-router.get('/:id', async (req, res, next) => {
-    try{
-        const products = await Product.findByPk(req.params.id)
-        res.send(products)
-    } catch (err){
-        next (err)
-    }
-})
-  
-router.post('/', async (req, res, next) => {
-    try{
-        const products = await Product.create(req.body)
-        res.send(products)
-    } catch (err){
-        next (err)
-    }
-})
-  
-router.put('/:id', async(req, res, next) => {
-    try{
-        const products = await Product.findByPk(req.params.id)
-        res.send(await products.update(req.body))
-    } catch(err) {
-        next(err)
-    }
-})
-  
-router.delete('/:id', async (req, res, next) => {
-    try{
-        const products = await Product.findByPk(req.params.id)
-        products.destroy()
-        res.send(products)
-    } catch (err){
-        next (err)
-    }
-})
+});
 
-module.exports = router
+router.post("/", async (req, res, next) => {
+    try {
+        const products = await Product.create(req.body);
+        res.send(products);
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.put("/:id", async (req, res, next) => {
+    try {
+        const products = await Product.findByPk(req.params.id);
+        res.send(await products.update(req.body));
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.delete("/:id", async (req, res, next) => {
+    try {
+        const products = await Product.findByPk(req.params.id);
+        products.destroy();
+        res.send(products);
+    } catch (err) {
+        next(err);
+    }
+});
+
+module.exports = router;
