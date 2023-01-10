e663307e480cf34aa07205f8c9c21646390da4f1
Bob <robert.dale.boles@gmail.com>
2023-01-10 13:25:39 -0700

    removed approutes and changed app.js

--- client/components/App.js
+++ client/components/App.js
@@ -1,15 +1,35 @@
-import React from 'react';
-
-import Navbar from './Navbar';
-import AppRoutes from './AppRoutes';
+import React from "react";
+import Navbar from "./Navbar";
+import { Route, Routes } from 'react-router-dom';
+import Home from './Home';
+import AuthForm from "./AuthForm";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { me } from '../store/store';
 
 const App = () => {
-  return (
-    <div>
-      <Navbar />
-      <AppRoutes />
-    </div>
-  );
+
+    const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(me());
+  }, []);
+
+    return (
+        <>
+            <div>
+                <Navbar />
+            </div>
+            <Routes>
+                <Route path="/*" element={<Home />} />
+                <Route to="/home" element={<Home />} />
+                <Route
+                    path="/login"
+                    element={<AuthForm name="login" displayName="Login" />}
+                />
+            </Routes>
+        </>
+    );
 };
 
 export default App;
