e897cb01fae02624e5df46938dcd1add3bb1a060
Armand Arslanian <aarslanian56@gmail.com>
2023-01-11 12:29:26 -0500

    singleProduct done, counter done

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,9 +1,12 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import { useParams, Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { selectSingleProduct, getSingleProduct } from "../slices/singleProductSlice";
 
 const SingleProduct = () => {
+    //this useState is ONLY for quantity counter
+    const [counter, setCounter] = useState(0);
+
     const {productId} = useParams()
     const dispatch = useDispatch()
 
@@ -19,6 +22,17 @@ const SingleProduct = () => {
         return <p>NO PRODUCTS FOUND</p>
     }
 
+    //Quantity Counter Logic
+    const increase = () => {
+        setCounter(count => count + 1);
+    };
+ 
+    const decrease = () => {
+        if (counter > 0) {
+          setCounter(count => count - 1);
+        }
+    };
+
     return(
         <div id='single-product'>
             <div id='single-product-info'>
@@ -28,6 +42,16 @@ const SingleProduct = () => {
                 <h3>Category: {category}</h3>
                 <p>Details: {description}</p>
                 <button>Add to Cart</button>
+                
+                <div className="quantityCounter">
+                    <h3>Quantity:</h3>
+                    <span className="quantityOutput">{counter}</span>
+                    <div className="btn-container">
+                        <button className="control__btn" onClick={increase}>+</button>
+                        <button className="control__btn" onClick={decrease}>-</button>
+                    </div>
+                </div>
+
             </div>
         </div>
     )
