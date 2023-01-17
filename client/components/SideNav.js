e90d1048a7db0767b6f86f82f44eb788f0127600
jmealor <jmealor@icloud.com>
2023-01-16 22:11:23 -0500

    added search box and it works

--- client/components/SideNav.js
+++ client/components/SideNav.js
@@ -3,25 +3,26 @@ import { Link } from 'react-router-dom'
 import { fetchProductsAsync, sortByCategory } from '../slices/allProductsSlice'
 import { useDispatch } from 'react-redux'
 
-const handleSubmit = (e) => {
-    console.log(e)
-    dispatch(fetchProductsAsync())
-    dispatch(sortByCategory())
-}
 
 function SideNav() {
     const dispatch = useDispatch();
+    const handleSubmit = async (e) => {
+        console.log(e)
+       await dispatch(fetchProductsAsync())
+       await dispatch(sortByCategory(e.target.value))
+    }
   return (
     <>
         <div className='sideNav'>
+          <button value='all' onClick={() => dispatch(fetchProductsAsync())}>All Products</button> 
           <button value='xbox' onClick={handleSubmit}>XBOX</button> 
-          <button value='wii' onClick={() => dispatch(sortByCategory('wii'))}>Wii</button> 
-          <button value='ps3' onClick={() => dispatch(sortByCategory('ps3'))}>PS3</button> 
-          <button value='ps2' onClick={() => dispatch(sortByCategory('Playstation 2'))}>PS2</button> 
-          <button value='playstation' onClick={() => dispatch(sortByCategory('Playstation'))}>Playstation</button> 
-          <button value='n64' onClick={() => dispatch(sortByCategory('Nintendo 64'))}>N64</button> 
-          <button value='snes' onClick={() => dispatch(sortByCategory('snes'))}>SNES</button> 
-          <button value='nes' onClick={() => dispatch(sortByCategory('NES'))}>NES</button> 
+          <button value='wii' onClick={handleSubmit}>Wii</button> 
+          <button value='ps3' onClick={handleSubmit}>PS3</button> 
+          <button value='Playstation 2' onClick={handleSubmit}>PS2</button> 
+          <button value='Playstation' onClick={handleSubmit}>Playstation</button> 
+          <button value='Nintendo 64' onClick={handleSubmit}>N64</button> 
+          <button value='snes' onClick={handleSubmit}>SNES</button> 
+          <button value='NES' onClick={handleSubmit}>NES</button> 
         </div>
     </>
   )
